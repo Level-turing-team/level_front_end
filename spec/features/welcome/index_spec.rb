84e2f3e4a32cdd2525988381163ff4c8e34f8f45
Khoa Nguyen <khoang323@hotmail.com>
2021-04-13 18:26:41 -0600

    Created user spec and welcome index spec
    Co-Authored-By: Joseph Budina <joebud3@gmail.com>

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -9,5 +9,22 @@ RSpec.describe 'Welcome Page' do
     it "they should be on the welcome page" do
       expect(current_path).to eq("/")
     end
+
+    it "they should see a welcome message" do
+      expect(page).to have_content("LEVEL")
+    end
+  end
+
+  describe "they should see a Button to Log in" do
+    it "And they can login with a correct username and password" do
+      user = User.create!(first_name: "Bob", last_name: "Fake", username: "funbucket13", email: 'funbucket13@gmail.com')
+      # within '.login-form' do
+        fill_in :username, with: user.username
+
+        click_on "Log In"
+      # end
+
+      expect(current_path).to eq(dashboard_path)
+    end
   end
 end
