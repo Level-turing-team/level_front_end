32339460820a17b0bab254c371ef2b47553f5185
Joseph Budina <joebud3@gmail.com>
2021-04-14 17:03:04 -0600

    Add registration and updates
    Co-authored-by: Khoa Nguyen <46826902+Omegaeye@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,14 @@
 class SessionsController < ApplicationController
-  def new; end
-
-  def create
-    user = User.find_by(username: params[:username])
-    if user
-      authenticate(user)
-    else
-      flash[:error] = 'Invalid username and password.'
-      redirect_to root_path
-    end
-  end
 
   def googleAuth
       access_token = request.env["omniauth.auth"]
       user = User.from_omniauth(access_token)
       session[:user_id] = user.google_id
-      if User.find_by(google_id: user.google_id)
-        redirect_to dashboard_index_path
-      else
+      user = User.find_by(google_id: user.google_id)
+      if user.username.nil? || user.zip.nil?
         redirect_to register_path
+      else
+        redirect_to dashboard_index_path
       end
     end
 
