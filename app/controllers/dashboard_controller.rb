28ab88b2690039311d914af58214a8e0331b1fc4
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-04-17 16:41:43 -0500

    Update controller action adding @ variables
    Co-authored-by: Kyle Schulz <54863905+kylejschulz@users.noreply.github.com>

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class DashboardController < ApplicationController
   def index
     @user = current_user
-
+    @user_profile_object = BackendFacade.profile_objects(@user.id)
+    @circle = User.find_circle(@user_profile_object.circle)
   end
 end
