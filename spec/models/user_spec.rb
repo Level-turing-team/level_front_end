af22c28c5c2ba6b70c14e8644fa6e0f7593bd622
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-04-17 16:56:22 -0500

    Add test for ::find_circle of User model.
    Co-authored-by: Kyle Schulz <54863905+kylejschulz@users.noreply.github.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,5 +25,33 @@ RSpec.describe User, type: :model do
                   })
       expect(User.from_omniauth(user)).to eq(User.first)
     end
+    describe "::find_circle" do
+      it "retreives user objects in an array from given data set" do
+        user1 = User.create!(id: 10000002, first_name: "Bob", last_name: "Jones", email: "somethingelse2@example.com",username: "test-user-24", google_id: "245234", zip: "80022", bio:"i love music and arts", picture_url: "http://www.ikozmik.com/Content/Images/uploaded/its-free-featured.jpg")
+        user2 = User.create!(id: 10000003, first_name: "Alexander", last_name: "Jones", email: "somethingelse3@example.com",username: "test-user-45", google_id: "245235", zip: "80023", bio:"Trying to connect with more artists")
+        user3 = User.create!(id: 10000004,first_name: "Sid", last_name: "Jones", email: "else@example.com",username: "test-user-457788", google_id: "44355676", zip: "80024", bio:"Photography is my passion")
+        user4 = User.create!(id: 10000005,first_name: "Simon", last_name: "Smith", email: "smith_else@example.com",username: "test-user-788", google_id: "12355676", zip: "80025", bio:"I love turtles")
+        data =  [{:id=>"17",
+           :type=>"profile",
+           :attributes=>{:zipcode=>"80022", :user_id=>10000002, :profile_picture=>"http://www.google.com", :username=>nil}},
+          {:id=>"18",
+           :type=>"profile",
+           :attributes=>{:zipcode=>"80023", :user_id=>10000003, :profile_picture=>"http://www.google.com", :username=>nil}},
+          {:id=>"19",
+           :type=>"profile",
+          :attributes=>{:zipcode=>"80024", :user_id=>10000004, :profile_picture=>"http://www.google.com", :username=>nil}},
+          {:id=>"20",
+           :type=>"profile",
+           :attributes=>{:zipcode=>"80025", :user_id=>10000005, :profile_picture=>"http://www.google.com", :username=>nil}}]
+
+           expect(User.find_circle(data).length).to eq(4)
+           expect(User.find_circle(data).class).to eq(Array)
+           expect(User.find_circle(data).first.class).to eq(User)
+           expect(User.find_circle(data).first).to eq(user1)
+           expect(User.find_circle(data).second).to eq(user2)
+           expect(User.find_circle(data).third).to eq(user3)
+           expect(User.find_circle(data).fourth).to eq(user4)
+      end
+    end
   end
 end
