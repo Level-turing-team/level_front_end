0e2572a8bfd0ebfda7987db8a8f8680caec60882
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-04-17 16:39:41 -0500

    Add seeds to match backend db
    Co-authored-by: Kyle Schulz <54863905+kylejschulz@users.noreply.github.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 @user_1 = User.create!(first_name: "Test", last_name: "Account", email: "something@example.com",username: "test-user", google_id: "123", zip: "12345",bio:"music is dope")
 @user_2 = User.create!(first_name: "Other", last_name: "Account", email: "somethingelse@example.com",username: "test-user-2", google_id: "456", zip: "23456",bio:"i love music")
+
+@user_3 = User.create!(id: 10000002, first_name: "Bob", last_name: "Jones", email: "somethingelse2@example.com",username: "test-user-24", google_id: "245234", zip: "80022", bio:"i love music and arts")
+@user_4 = User.create!(id: 10000003, first_name: "Alexander", last_name: "Jones", email: "somethingelse3@example.com",username: "test-user-45", google_id: "245235", zip: "80023", bio:"Trying to connect with more artists")
+@user_5 = User.create!(id: 10000004,first_name: "Sid", last_name: "Jones", email: "else@example.com",username: "test-user-457788", google_id: "44355676", zip: "80024", bio:"Photography is my passion")
+@user_6 = User.create!(id: 10000005,first_name: "Simon", last_name: "Smith", email: "smith_else@example.com",username: "test-user-788", google_id: "12355676", zip: "80025", bio:"I love turtles")
+
