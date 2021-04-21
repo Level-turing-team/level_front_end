e861b9c5624020489f0c70056c3092beee3ede50
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-04-21 18:02:55 -0500

    Add tests for search functionality w radio buttons.

--- spec/features/discover/users_search_spec.rb
+++ spec/features/discover/users_search_spec.rb
@@ -9,15 +9,47 @@ RSpec.describe 'As an authenticated user' do
     it 'searches by name' do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
       visit discover_index_path(@user_1)
-      save_and_open_page
 
+      page.choose('option1')
       fill_in 'search_term', with: 'korn'
-      click_button 'Search'
+      click_button 'commit'
 
       expect(dashboard_index_path)
-      within('.search_card') do
+      within('.results') do
         expect(page).to have_content('korn')
         expect(page).to_not have_content('slipknot')
+      end
+    end
+    it 'searches by distance' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
+      visit discover_index_path(@user_1)
+
+      page.choose('option2')
+      fill_in 'search_term', with: '50'
+      click_button 'commit'
+      expect(dashboard_index_path)
+      within('.results') do
+        expect(page).to have_content('slipknot')
+        expect(page).to have_content('photos by jim')
+        expect(page).to have_content('arteest')
+        expect(page).to have_content('former salesman')
+        expect(page).to have_content('korn')
+        expect(page).to have_content('Pie')
+      end
+    end
+    it 'searches by tag' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
+      visit discover_index_path(@user_1)
+
+      page.choose('option3')
+      fill_in 'search_term', with: 'Painter'
+      click_button 'commit'
+      expect(dashboard_index_path)
+      within('.results') do
+        expect(page).to have_content('slipknot')
+        expect(page).to have_content('photos by jim')
+        expect(page).to have_content('former salesman')
+        expect(page).to_not have_content('korn')
       end
     end
   end
