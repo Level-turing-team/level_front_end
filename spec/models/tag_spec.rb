76dfa14667153d6eb55c2909aa3ebc008d0a6046
kylejschulz <kylschulz@gmail.com>
2021-04-21 19:13:37 -0400

    make photo a link in recent photos discover page
    Co-authored-by: Andrew Johnston <72912074+avjohnston@users.noreply.github.com>
    Co-authored-by: Ben Fulton <63377504+b-enji-cmd@users.noreply.github.com>
    Co-authored-by: Kris Litman <56943408+krislitman@users.noreply.github.com>

---
+++ spec/models/tag_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Tag, type: :model do
+  describe "Tag poro" do
+    it "creates a tag poro with the right data" do
+      data = {
+              attributes: {
+                          name: "painter"
+                          }
+              }
+      @tag = Tag.new(data)
+
+      expect(@tag.name).to eq("painter")
+    end
+  end
+end
