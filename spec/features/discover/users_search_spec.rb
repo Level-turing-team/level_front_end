44b27198513e92090ebf45527bf5db003dbf8740
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-04-21 18:13:51 -0500

    Add sad path tests for not finding records with the given criteria
    Co-authored-by: Joseph Budina <52386927+josephbudina@users.noreply.github.com>

--- spec/features/discover/users_search_spec.rb
+++ spec/features/discover/users_search_spec.rb
@@ -53,4 +53,61 @@ RSpec.describe 'As an authenticated user' do
       end
     end
   end
+  describe ' sad path it searches by name, distance, or tag', :vcr do
+    it 'has 0 results if no name found' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
+      visit discover_index_path(@user_1)
+
+      page.choose('option1')
+      fill_in 'search_term', with: 'aldkjs'
+      click_button 'commit'
+
+      expect(dashboard_index_path)
+      within('.results') do
+        expect(page).to_not have_content('slipknot')
+        expect(page).to_not have_content('photos by jim')
+        expect(page).to_not have_content('arteest')
+        expect(page).to_not have_content('former salesman')
+        expect(page).to_not have_content('korn')
+        expect(page).to_not have_content('Pie')
+        expect(page).to have_content('You have 0 results')
+      end
+    end
+    it 'has 0 results if there are no users within distance given' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
+      visit discover_index_path(@user_1)
+
+      page.choose('option2')
+      fill_in 'search_term', with: '1'
+      click_button 'commit'
+      expect(dashboard_index_path)
+      within('.results') do
+        expect(page).to_not have_content('slipknot')
+        expect(page).to_not have_content('photos by jim')
+        expect(page).to_not have_content('arteest')
+        expect(page).to_not have_content('former salesman')
+        expect(page).to_not have_content('korn')
+        expect(page).to_not have_content('Pie')
+        expect(page).to have_content('You have 0 results')
+      end
+    end
+    it 'has 0 results if there are no artists with the tag provided' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
+      visit discover_index_path(@user_1)
+
+      page.choose('option3')
+      fill_in 'search_term', with: 'Singer'
+      click_button 'commit'
+      expect(dashboard_index_path)
+      within('.results') do
+        expect(page).to_not have_content('slipknot')
+        expect(page).to_not have_content('photos by jim')
+        expect(page).to_not have_content('arteest')
+        expect(page).to_not have_content('former salesman')
+        expect(page).to_not have_content('korn')
+        expect(page).to_not have_content('Pie')
+        expect(page).to have_content('You have 0 results')
+      end
+    end
+  end
 end
