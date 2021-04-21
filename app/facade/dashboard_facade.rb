41efd4713dcd24a1d95df134760eab5ff4dde407
Joseph Budina <joebud3@gmail.com>
2021-04-21 17:04:47 -0600

    Add profile photo functionality and gallery functionality
    Co-authored-by: Andrew Johnston <72912074+avjohnston@users.noreply.github.com>

--- app/facade/dashboard_facade.rb
+++ app/facade/dashboard_facade.rb
@@ -6,13 +6,15 @@ class DashboardFacade < BackendFacade
     profile_photo = BackendService.get_user(user_id)[:data][:attributes][:profile_picture]
     username = BackendService.get_user(user_id)[:data][:attributes][:username]
     tags = tag_objects(user_id)
+    galleries = gallery_objects(user_id)
     Profile.new({
       id: user_id,
       username: username,
       circle: user_circle,
       circle_posts: circle_posts,
       profile_photo: profile_photo,
-      tags: tags
+      tags: tags,
+      galleries: galleries
     })
   end
 end
