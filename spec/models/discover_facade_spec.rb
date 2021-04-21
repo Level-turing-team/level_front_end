c2230de7a9eb8153683cd57a07418c4a33ad9bea
kylejschulz <kylschulz@gmail.com>
2021-04-21 16:50:23 -0400

    Add finishing touches to discover_facade
    Co-authored-by: Andrew Johnston <72912074+avjohnston@users.noreply.github.com>
    Co-authored-by: Ben Fulton <63377504+b-enji-cmd@users.noreply.github.com>
    Co-authored-by: Jake Volpe <62768837+javolpe@users.noreply.github.com>
    Co-authored-by: Jesus Quezada-Guillen <17693874+jequeza@users.noreply.github.com>
    Co-authored-by: Joseph Budina <52386927+josephbudina@users.noreply.github.com>
    Co-authored-by: Khoa Nguyen <46826902+Omegaeye@users.noreply.github.com>
    Co-authored-by: Kris Litman <56943408+krislitman@users.noreply.github.com>

--- spec/models/discover_facade_spec.rb
+++ spec/models/discover_facade_spec.rb
@@ -13,27 +13,29 @@ RSpec.describe DiscoverFacade do
 
     it "#recent_photo_objects", :vcr do
       result = DiscoverFacade.recent_photo_objects
+      expect(result.first).to be_a(Photo)
+      expect(result.length).to eq(22)
+      expect(result.first.description).to eq("pic 1")
+      expect(result.first.gallery_id).to eq(8)
+      expect(result.first.url).to eq("http:www.google.com")
     end
 
     it "#search_user_objects", :vcr do
       result = DiscoverFacade.search_user_objects(1, "slipknot", "name")
-      # require "pry"; binding.pry
-      expect(result).to be_a(Profile)
-      expect(result.profile_photo).to eq("http://www.google.com")
-      expect(result.username).to eq("slipknot")
-      expect(result.zipcode).to eq("80301")
-      expect(result.id).to eq(1)
+
+      expect(result.first).to be_a(Profile)
+      expect(result.first.profile_photo).to eq("http://www.google.com")
+      expect(result.first.username).to eq("slipknot")
+      expect(result.first.zipcode).to eq("80304")
+      expect(result.first.id).to eq("3")
     end
 
     it "artists_near_me", :vcr do
       result = DiscoverFacade.artists_near_me(1)
-
-      expect(result).to be_an_instance_of(Profile)
+      expect(result.first).to be_an_instance_of(Profile)
       expect(result.length).to eq(4)
       expect(result.first.username).to eq("korn")
       expect(result.first.zipcode).to eq("80305")
-
-      require "pry"; binding.pry
     end
   end
 end
