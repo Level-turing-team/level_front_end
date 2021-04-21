36e99217f3c57e1c45d738af6ae07f1f638909b7
kylejschulz <kylschulz@gmail.com>
2021-04-21 16:35:11 -0400

    Add more class method testing in the dashboard_facade
    Co-authored-by: Andrew Johnston <72912074+avjohnston@users.noreply.github.com>
    Co-authored-by: Ben Fulton <63377504+b-enji-cmd@users.noreply.github.com>
    Co-authored-by: Joseph Budina <52386927+josephbudina@users.noreply.github.com>
    Co-authored-by: Kris Litman <56943408+krislitman@users.noreply.github.com>

--- spec/models/discover_facade_spec.rb
+++ spec/models/discover_facade_spec.rb
@@ -10,5 +10,30 @@ RSpec.describe DiscoverFacade do
       expect(result.zipcode).to eq("80301")
       expect(result.id).to eq(1)
     end
+
+    it "#recent_photo_objects", :vcr do
+      result = DiscoverFacade.recent_photo_objects
+    end
+
+    it "#search_user_objects", :vcr do
+      result = DiscoverFacade.search_user_objects(1, "slipknot", "name")
+      # require "pry"; binding.pry
+      expect(result).to be_a(Profile)
+      expect(result.profile_photo).to eq("http://www.google.com")
+      expect(result.username).to eq("slipknot")
+      expect(result.zipcode).to eq("80301")
+      expect(result.id).to eq(1)
+    end
+
+    it "artists_near_me", :vcr do
+      result = DiscoverFacade.artists_near_me(1)
+
+      expect(result).to be_an_instance_of(Profile)
+      expect(result.length).to eq(4)
+      expect(result.first.username).to eq("korn")
+      expect(result.first.zipcode).to eq("80305")
+
+      require "pry"; binding.pry
+    end
   end
 end
